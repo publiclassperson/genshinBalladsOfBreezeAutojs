64b472cd568b60152bfa6d4a345c8fd10bfadf9a
mono <935687014@qq.com>
2021-03-29 01:57:24 +0800

    [MODIFY]重构代码

--- play.js
+++ play.js
@@ -1,28 +1,9 @@
-
 auto();
 requestScreenCapture(false);
-console.show();
 //检查获取截图权限与无障碍权限 
 //游戏记得调低帧率24/30
-
-// 比例形式 way = 0(楼主按自己的平板比例来的等待测试)
-// 坐标模式 way = 1(需要自己定位)
-var way = 0;
-
-//比例形式
-var adresses = {
-    ru: [0.775, 0.485],
-    rr: [0.882, 0.662],
-    rd: [0.771, 0.833],
-    rl: [0.664, 0.662],
-    lu: [0.225, 0.485],
-    lr: [0.336, 0.662],
-    ld: [0.225, 0.833],
-    ll: [0.114, 0.662]
-}
-
 //找出八个白色四角星中心点坐标 截图自己找对应点楼主2800*1752;
-var adressesX = {
+const addresses = {
     ru: [2170, 850]
     , rr: [2470, 1160]
     , rd: [2160, 1460]
@@ -32,120 +13,86 @@ var adressesX = {
     , ld: [630, 1460]
     , ll: [320, 1150]
 }
-
-
+//初始化随机延迟
+const delay = random(450, 460)
 //中间颜色色值
-var colorTap = "#fffbf2";
-
-//获取设备宽度
-var devWidth = device.width;
-//获取设备高度
-var devHeight = device.height;
-
+const destColor = "#fffbf2";
+//相似度
+const similar = 4;
 
 // 循环跑起来
-if (way == 0) {
-    while (true) {
-        presstap(adresses);
-    }
-} else {
-    while (true) {
-        presstapX(adressesX);
-    }
+while (true) {
+    let points = findWhitePoint();
+    points.length > 0 && ClickAll(points);
 }
 
-//遍历八个点,找出符合颜色的点，并点击 
-function presstap(tap) {
 
-    // var time1 = new Date()
-    // 初始化可点击对象
-    var arr = []
-    var cS = captureScreen();
-    var uu = random(280, 330)
+function consoleSHOW() {
+    //获取设备宽度
+    var devWidth = device.width;
+    //获取设备高度
+    var devHeight = device.height;
 
+    //设置控制台显示大小与位置
+    console.show();
+    console.setSize(500, 500);
+    console.setPosition(devWidth / 16, devHeight / 16);
+    // n = console.rawI11112nput("请输入数字1开始弹琴:")
+}
+
+//等待原神打开
+function waitForYuanshen() {
+    const activityPackage = "com.miHoYo.Yuanshen"
+    const activityPages = "com.miHoYo.GetMobileInfo.MainActivity";
+    const playPage = "com.afollestad.materialdialogs.MaterialDialog";
+
+    waitForPackage(activityPackage, 3000)
+    // waitForActivity(activityPages, 3000)
+    // waitForActivity(playPage, 3000)
+}
+//遍历八个点,找出符合颜色的点 
+function findWhitePoint() {
+    let arr = []
     // 遍历八个点，查找符合颜色的点
-    for (var k in tap) {
-        var key = tap[k];
-        var x = parseInt(key[0] * devWidth);
-        var y = parseInt(key[1] * devHeight);
-        var c1 = cS.pixel(x, y)
-        var c2 = colors.parseColor(colorTap);
+    let cs = captureScreen()
+    for (let k in addresses) {
+        let x = addresses[k][0];
+        let y = addresses[k][1];
 
-        if (colors.isSimilar(c1, c2, 4)) {
-            var rd = random(-20, 20)
-            // rd = 0
-            //组装出多点数组
-            arr.push({ x: x + rd, y: y - rd })
-
-        }
+        let flag = similarCheck(cs, x, y);
+        let rd = random(-10, 10)
+        flag && arr.push({ x: x + rd, y: y + rd });
     }
     //如果可点击的数组容量大于0，点起来
+    return arr ? arr : [];
 
-    if (arr.length > 0) {
-        // var time2 = new Date()
-        // log(time2 - time1)
-        log("找到" + arr.length + "个点需要点！")
-        //组装点击参数数组
-        var arrClick = []
-        for (var i = 0; i < arr.length; i++) {
-
-            arrClick.push([0, uu, [arr[i].x, arr[i].y],])
-
-        }
-        // var time3 = new Date()
-        // log(time3 - time1)
-
-        //多点操作
-        // log(arrClick)
-        // gesture(arrClick)
-        gestures.apply(null, arrClick)
-        // var time4 = new Date()
-        // log(time4 - time1)
-
+    /**
+     * @param {坐标x} x 
+     * @param {坐标y} y 
+     */
+    function similarCheck(x, y) {
+        //点击队列
+        let c1 = cs.pixel(x, y);
+        let c2 = colors.parseColor(destColor);
+        return colors.isSimilar(c1, c2, similar)
     }
+
 }
-//遍历八个点,找出符合颜色的点，并点击 
-function presstapX(tap) {
-
-    // var time1 = new Date()
-    // 初始化可点击对象
-    var arr = []
-    var cS = captureScreen();
-    var uu = random(280, 330)
-
-    // 遍历八个点，查找符合颜色的点
-    for (var k in tap) {
-        var key = tap[k];
-        var x = key[0];
-        var y = key[1];
-
-        var c1 = cS.pixel(x, y)
-        var c2 = colors.parseColor(colorTap);
-
-        if (colors.isSimilar(c1, c2, 4)) {
-            var rd = random(-20, 20)
-            //组装出多点数组
-            arr.push({ x: parseInt(x) + rd, y: parseInt(y) - rd })
-
-        }
-    }
-    //如果可点击的数组容量大于0，点起来
-    if (arr.length > 0) {
-        // var time2 = new Date()
-        // log(time2 - time1)
-        log("找到" + arr.length + "个点需要点！")
+//点击所有可点击点
+function ClickAll(arrp) {
+    let parmsize = arrp.length;
+    if (parmsize > 0) {
+        console.time("ClickAll")
         //组装点击参数数组
-        var arrClick = []
-        for (var i = 0; i < arr.length; i++) {
-            arrClick.push([0, uu, [arr[i].x, arr[i].y],])
+        let arrClick = [];
+        log("找到" + parmsize + "个点需要点！");
+        for (let i = 0; i < parmsize; i++) {
+            arrClick.push([0, delay, [arrp[i].x, arrp[i].y],]);
         }
-        // var time3 = new Date()
-        // log(time3 - time1)
-
         //多点操作
-        gestures.apply(null, arrClick);
-        // var time4 = new Date()
-        // log(time4 - time1)
-
+        arrClick.length > 0 && gestures.apply(null, arrClick);
+        console.timeEnd("ClickAll")
     }
+
 }
+
